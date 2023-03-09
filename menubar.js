40096eda9074a9d08a45c79063e211c1d84eddd7
Justin  Cambria <justincambria@Justins-MacBook-Air.local>
2023-03-09 18:53:24 -0500

    updated webpage

--- menubar.js
+++
@@ -1,9 +0,0 @@
-function myfunction (){
-    var x = document.getElementById("links");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
-
