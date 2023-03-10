a9a588d2c23e6d202c4152575c39909001b9c3a8
Justin  Cambria <justincambria@Justins-MacBook-Air.local>
2023-03-10 12:50:40 -0500

    cleaned code, added js game

---
+++ function.js
@@ -0,0 +1,41 @@
+// Function to get the computer's choice
+function getComputerChoice() {
+    var choices = ['rock', 'paper', 'scissors'];
+    var randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+  }
+  
+  // Function to determine the winner
+  function determineWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+      return "It's a tie, great minds think alike!";
+    } else if (userChoice === 'rock' && computerChoice === 'paper') {
+      return "I win!";
+    } else if (userChoice === 'paper' && computerChoice === 'scissors') {
+      return "I win!";
+    } else if (userChoice === 'scissors' && computerChoice === 'rock') {
+      return "I win!";
+    } else {
+      return "You win! Best two out if three?";
+    }
+  }
+  
+  // Function to play the game
+  function playGame(userChoice) {
+    var computerChoice = getComputerChoice();
+    var result = determineWinner(userChoice, computerChoice);
+    document.getElementById('result').innerHTML = result;
+  }
+  
+  // Event listeners for the user's choice
+  document.getElementById('rock').addEventListener('click', function() {
+    playGame('rock');
+  });
+  
+  document.getElementById('paper').addEventListener('click', function() {
+    playGame('paper');
+  });
+  
+  document.getElementById('scissors').addEventListener('click', function() {
+    playGame('scissors');
+  });
